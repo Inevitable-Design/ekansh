e9af0f549a9baf02d42454b2ddbcc695d49b723c
Ekansh <96431359+Inevitable-Design@users.noreply.github.com>
2024-04-26 00:25:33 +0530

    Inevitable-Cli Ver. 1.0.6

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import figlet from 'figlet';
+import { intro, outro, select, spinner  } from '@clack/prompts';
+import data from './lib/data.js';
+import open from 'open';
+import fs from 'fs';
+import path from 'path';
+import lolcatjs from 'lolcatjs';
 
-console.log('doing business')
+const __dirname = './static/fonts/'
+let fontData = fs.readFileSync(path.join(__dirname, "3d.flf"), "utf8");
+
+// Function to display ASCII art
+function displayAsciiArt() {
+    figlet.parseFont("3d", fontData)
+    lolcatjs.options.animate = true;
+    lolcatjs.fromString(figlet.textSync(`${data.cli}`, "3d") + `\n\nVersion: ${data.version} | Made with love by ${data.author}`);
+}
+
+function displayUserBio() {
+    lolcatjs.fromString(`\n${data.bio}\n${data.title.join(' | ')}\n`);
+}
+
+// Function to display social links
+function displaySocials() {
+    console.log(chalk.yellow('Ways to connect with me:'));
+    data.urls.forEach(({ label, value, username }) => {
+        const display = value ? value : ``;
+        console.log(`${chalk.blue(label)}: ${chalk.cyan(username)} | ${chalk.green(display)}`);
+    });
+    console.log(`\n`);
+}
+
+async function displayQuickLaunchLinks() {
+    const options = data.urls.map(({ label, value, hint }) => ({ label, value, hint }));
+    const selectedUrl = await select({
+        message: 'Pick a link to open:',
+        options: options,
+    });
+    if (selectedUrl) {
+        const s = spinner();
+        await s.start('Opening Link');
+        await open(selectedUrl);
+        s.stop('Link Opened!');
+        // console.log(`Opened ${selectedUrl}`);
+    } else {
+        console.log('No URL selected.');
+    }
+}
+
+
+// Main function
+// Main function
+async function main() {
+    let exit = false;
+    while (!exit) {
+        displayAsciiArt();
+        displayUserBio();
+        displaySocials();
+
+        intro(`Quick Launch Links`);
+        await displayQuickLaunchLinks();
+        outro(`Press 'e' to exit or any other key to continue.`);
+        
+        const exitOption = await select({
+            message: 'Your choice:',
+            options: [
+                { label: 'Exit', value: 'e' },
+                { label: 'Continue', value: 'c' }
+            ]
+        });
+
+        if (exitOption === 'e') {
+            exit = true;
+            lolcatjs.fromString('Exiting...')
+        }
+    }
+}
+
+// Run the main function
+main();
